462680780f9836d2737427c1447b740049634904
Kaka <vn503024@gmail.com>
2024-04-16 02:41:00 +0800

    feat: add the speech recognition app that make by gradio

---
+++ audio_recognition_app.py
@@ -0,0 +1,30 @@
+import gradio as gr
+from transformers import pipeline
+
+
+model = pipeline("automatic-speech-recognition")
+
+
+def transcribe_audio(mic=None, file=None):
+    if mic is not None:
+        audio = mic
+    elif file is not None:
+        audio = file
+    else:
+        return "You must either provide a mic recording or a file"
+    transcription = model(audio)["text"]
+    return transcription
+
+
+if __name__ == "__main__":
+    app = gr.Interface(
+        fn=transcribe_audio,
+        inputs=[
+            gr.Audio(sources="microphone", type="filepath"),
+            gr.Audio(sources="upload", type="filepath"),
+        ],
+        outputs="text",
+    )
+
+
+    app.launch()
